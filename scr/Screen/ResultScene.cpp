b5e3eeb5655f96409c113e18956d217aeae73e45
302-17\koyo <kobe-license@kobe-tech.ac.jp>
2025-12-03 15:19:32 +0900

    メソッドの用意

--- scr/Screen/ResultScene.cpp
+++ scr/Screen/ResultScene.cpp
@@ -1,1 +1,21 @@
 #include "ResultScene.h"
+
+void ResultScene::Init()
+{
+}
+
+void ResultScene::handleEvent()
+{
+}
+
+void ResultScene::Update()
+{
+}
+
+void ResultScene::Render(sf::RenderWindow& window)
+{
+}
+
+void ResultScene::End()
+{
+}
